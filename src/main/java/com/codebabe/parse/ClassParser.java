9fb979f3acd3053b0920a0d17a4eaa5cca5824d2
codebabe <codebabe.go@gmail.com>
2016-11-29 22:55:49 +0800

    定义几个接口

---
+++ src/main/java/com/codebabe/parse/ClassParser.java
@@ -0,0 +1,22 @@
+package com.codebabe.parse;
+
+/**
+ * author: code.babe
+ * date: 2016-11-29 20:23
+ */
+public abstract class ClassParser {
+
+    /**
+     * 测试的方法
+     * @param clz
+     * @param methodName
+     * @param args
+     */
+    protected void go4Testing(Class clz, String methodName, Object... args) {
+        if (clz == null) {
+            return;
+        }
+
+    }
+
+}
